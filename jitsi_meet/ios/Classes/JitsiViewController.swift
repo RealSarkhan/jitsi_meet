281a73c2a006c77aad19da3482275759edc6836a
Real Sarkhan <rserxanbeyli@creativeadvtech.com>
2023-06-06 11:48:41 +0400

    version updated

--- jitsi_meet/ios/Classes/JitsiViewController.swift
+++ jitsi_meet/ios/Classes/JitsiViewController.swift
@@ -63,14 +63,15 @@ class JitsiViewController: UIViewController {
         jitsiMeetView.delegate = self
         self.jitsiMeetView = jitsiMeetView
         let options = JitsiMeetConferenceOptions.fromBuilder { (builder) in
-            builder.welcomePageEnabled = true
+//            builder.welcomePageEnabled = true
+        
             builder.room = self.roomName
             builder.serverURL = self.serverUrl
-            builder.subject = self.subject
+            builder.setSubject(self.subject ?? "")
             builder.userInfo = self.jistiMeetUserInfo
-            builder.audioOnly = self.audioOnly ?? false
-            builder.audioMuted = self.audioMuted ?? false
-            builder.videoMuted = self.videoMuted ?? false
+            builder.setAudioMuted(self.audioMuted ?? false)
+            builder.setAudioOnly(self.audioOnly ?? false)
+            builder.setVideoMuted(self.videoMuted ?? false)
             builder.token = self.token
             
             self.featureFlags?.forEach{ key,value in
